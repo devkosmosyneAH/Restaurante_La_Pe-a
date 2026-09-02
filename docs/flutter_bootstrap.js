036d95fd4188ef7a82ccb4ef54c0873411b72ee6
18-anth <anthonycordova330@gmail.com>
2026-09-02 16:15:21 -0500

    ya vale madres

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,14 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3271806056"
+<<<<<<< HEAD
+    serviceWorkerVersion: "461229074"
+=======
+<<<<<<< HEAD
+    serviceWorkerVersion: "2814430243"
+=======
+    serviceWorkerVersion: "461229074"
+>>>>>>> 4770b88 (first commit)
+>>>>>>> 32e798b (first commit)
   }
 });
